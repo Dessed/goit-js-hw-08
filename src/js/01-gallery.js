674080b41d0aba02573b2867d54a66ab5909a0d3
Dessed <des.ztrcnto@gmail.com>
2022-08-14 22:15:45 +0300

    SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const fotoMarkup = creatingImageGallery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', fotoMarkup);
+
+function creatingImageGallery(pictures) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    }).join('');
+};
+
+gallery.addEventListener('click', onGallerySimpleLightbox);
+
+function onGallerySimpleLightbox (evt) { 
+  evt.preventDefault();
+  
+  let lightbox = new SimpleLightbox('.gallery a',
+   {captionsData: 'alt',
+    captionDelay: 250,
+    navText: ['←','→']
+    });
+};
+
