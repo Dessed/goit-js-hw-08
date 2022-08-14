783f4d99d716a9d079a202d92f9dce325122ec33
Dessed <des.ztrcnto@gmail.com>
2022-08-14 23:12:48 +0300

    Video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
 
+
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+player.on('timeupdate', function(evt) {
+    console.log(evt);
+    localStorage.setItem("videoplayer-current-time", evt);
+});
+
+
+// player.getCurrentTime().then(function(seconds) {
+//     // seconds = the current playback position
+// }).catch(function(error) {
+//     // an error occurred
+// });
