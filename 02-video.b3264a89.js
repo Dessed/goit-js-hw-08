fa4304b9ff5853673eb4725f6ba7359f6357a23f
Dessed <87389570+Dessed@users.noreply.github.com>
2022-08-24 11:35:24 +0000

    Deploying to gh-pages from @ Dessed/goit-js-hw-08@eb84195b4f3c44217ee884aeb5ae09e23961465c 🚀

--- 02-video.b3264a89.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)})),o.on("timeupdate",(function(e){console.log(e),localStorage.setItem("videoplayer-current-time",e)}))}();
-//# sourceMappingURL=02-video.b3264a89.js.map
